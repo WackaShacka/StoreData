1187139023bb953af5692c550ab7c5233f202a69
William <ambrosewilliam@ymail.com>
2018-05-14 15:31:20 -0700

    Initial Commit

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
 var multer = require('multer');
-bar cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');
 
 var router = express.Router();
 
